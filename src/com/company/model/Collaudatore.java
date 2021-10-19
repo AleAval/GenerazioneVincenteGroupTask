6cd5319659db31ac3f9cf3b004311fa1f1975564
Void-Frankie <87224914+Void-Frankie@users.noreply.github.com>
2021-10-19 16:10:36 +0200

    Collaudatore

--- src/com/company/model/Collaudatore.java
+++ src/com/company/model/Collaudatore.java
@@ -1,76 +1,88 @@
 package com.company.model;
 
-//FRANCESCO FARO
+/**
+ * @author Francesco Faro
+ */
 
 import com.company.costant.Area;
 
 public class Collaudatore implements User {
 
-    private final String nome;
-    private final String cognome;
-    private String mail;
-    private Area area;
-    private Task task;
+	private final String nome;
+	private final String cognome;
+	private String mail;
+	private Area area;
+	private Task task;
 
-    public Collaudatore(String nome, String cognome, String mail, Area area, Task task) {
-        this.nome = nome;
-        this.cognome = cognome;
-        this.mail = mail;
-        this.area = area;
-        this.task = task;
-    }
+	public Collaudatore(String nome, String cognome, String mail, Area area, Task task) 
+	{
+		this.nome = nome;
+		this.cognome = cognome;
+		this.mail = mail;
+	}
 
-    //----------------------------------------------------------------------------------------------------------------//
+	//----------------------------------------------------------------------------------------------------------------//
 
-    public void setMail(String mail) {
-        this.mail = mail;
-    }
+	public void setMail(String mail) 
+	{
+		this.mail = mail;
+	}
 
-    public Area getArea() {
-        return area;
-    }
+	public Area getArea() 
+	{
+		return area;
+	}
 
-    public void setArea(Area area) {
-        this.area = area;
-    }
+	public void setArea(Area area) 
+	{
+		this.area = area;
+	}
 
-    public Task getTask() {
-        return task;
-    }
+	public Task getTask() 
+	{
+		return task;
+	}
 
-    public void setTask(Task task) {
-        this.task = task;
-    }
+	public void setTask(Task task) 
+	{
+		this.task = task;
+	}
 
-    //----------------------------------------------------------------------------------------------------------------//
+	//----------------------------------------------------------------------------------------------------------------//
 
-    @java.lang.Override
-    public String getNome() {
-        return null;
-    }
+	@java.lang.Override
+	public String getNome() 
+	{
+		return null;
+	}
 
-    @java.lang.Override
-    public String getCognome() {
-        return null;
-    }
+	@java.lang.Override
+	public String getCognome() 
+	{
+		return null;
+	}
 
-    @java.lang.Override
-    public String getMail() {
-        return null;
-    }
+	@java.lang.Override
+	public String getMail() 
+	{
+		return null;
+	}
 
-    @java.lang.Override
-    public Area appartiene() {
-        return null;
-    }
+	@java.lang.Override
+	public Area appartiene() 
+	{
+		return null;
+	}
 
-    @java.lang.Override
-    public Task taskSvolta() {
-        return null;
-    }
+	@java.lang.Override
+	public Task taskSvolta() 
+	{
+		return null;
+	}
 
-    @java.lang.Override
-    public boolean allocatoSuTask() {
-        return false;
-    }
+	@java.lang.Override
+	public boolean allocatoSuTask() 
+	{
+		return false;
+	}
 }
